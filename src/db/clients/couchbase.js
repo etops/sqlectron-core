2272756406eba1b580e42d486069be84d3697d83
Christian Käslin <c.kaeslin@gmx.ch>
2016-05-20 14:34:04 +0200

    add additional features (ottoman fixed currently)

--- src/db/clients/couchbase.js
+++ src/db/clients/couchbase.js
@@ -1,5 +1,5 @@
 import couchbase from 'couchbase';
-
+const N1qlQuery = couchbase.N1qlQuery;
 const debug = require('../../debug')('db:clients:couchbase');
 
 export default function(server, database) {
@@ -15,49 +15,62 @@ export default function(server, database) {
         throw err;
       }
       let manager = cluster.manager();
+      let bucketManager = bucket.manager();
       bucket.enableN1ql(dbConfig.host);
       debug('connnected');
       resolve({
         wrapQuery,
         disconnect: () => disconnect(bucket),
-        listTables: () => listTables(manager),
-        executeQuery: (query) => executeQuery(bucket, query),
+        listTables: () => listTables(bucket, dbConfig.database),
+        executeQuery: (query) => executeQuery(bucket, query, dbConfig.database),
 
-        listViews: () => listViews(),
+        listViews: () => listViews(bucketManager),
         listRoutines: () => listRoutines(),
-        listTableColumns: (table) => listTableColumns(),
+        listTableColumns: (table) => listTableColumns(bucket, table, dbConfig.database),
         listTableTriggers: (table) => listTableTriggers(),
-        listDatabases: () => listDatabases(dbConfig.database),
-        getQuerySelectTop: (table, limit) => getQuerySelectTop(),
+        listDatabases: () => listDatabases(manager),
+        getQuerySelectTop: (table, limit) => getQuerySelectTop(dbConfig.database, table, limit),
         getTableCreateScript: (table) => getTableCreateScript(),
         getViewCreateScript: (view) => getViewCreateScript(),
         getRoutineCreateScript: (routine) => getRoutineCreateScript(),
         truncateAllTables: () => truncateAllTables(),
-
       });
-
     });
-
   });
 }
-
-
-
 
 export function disconnect(bucket) {
   bucket.disconnect();
 }
 
-export function listTables(manager) {
+export function listTables(bucket, bucketName) {
   return new Promise((resolve, reject) => {
-    manager.listBuckets(function(err, buckets) {
-      resolve(buckets.map(bucket => bucket.name));
-    });
+    const sql = `
+      SELECT _type
+      FROM ${bucketName}
+      GROUP BY _type
+    `;
+    try {
+      var query = N1qlQuery.fromString(sql);
+      bucket.query(query, function(err, res) {
+        if (err) {
+          reject(err);
+        }
+        //console.log(res);
+        //console.log(res.map(row => row._type));
+        resolve(res.map(row => row._type).sort());
+      });
+    } catch(err) {
+      reject(err);
+    }
   });
 }
 
-export function listViews() {
+export function listViews(bucketManager) {
   return new Promise((resolve, reject) => {
+    //var docs = bucketManager.getDesignDocuments();
+    // get design documents and for each design documents all the views
+    // console.log(docs);
     resolve([]);
   });
 }
@@ -68,9 +81,25 @@ export function listRoutines() {
   });
 }
 
-export function listTableColumns() {
+export function listTableColumns(bucket, table, bucketName) {
   return new Promise((resolve, reject) => {
-    resolve([]);
+    const sql = `
+      SELECT *
+      FROM ${bucketName}
+      WHERE _type="${table}"
+      LIMIT 1
+    `;
+    try {
+      var query = N1qlQuery.fromString(sql);
+      bucket.query(query, function(err, res) {
+        if (err) {
+          reject(err);
+        }
+        resolve(Object.keys(res[0][bucketName]).map(key => ({ columnName: key, dataType: typeof res[0][bucketName][key]})));
+      });
+    } catch(err) {
+      reject(err);
+    }
   });
 }
 
@@ -80,53 +109,57 @@ export function listTableTriggers() {
   });
 }
 
-export function executeQuery(bucket, queryString) {
+export function executeQuery(bucket, queryString, bucketName) {
+  let name = getFrom(queryString, bucketName);
   return new Promise((resolve, reject) => {
-    const N1qlQuery = couchbase.N1qlQuery;
-    var query = N1qlQuery.fromString(queryString);
-    bucket.query(query, function(err, res) {
-      if (err) {
-        console.log('query failed', err);
-        return;
-      }
-      console.log(res);
-      let ba = [parseRowQueryResult(res)];
-      console.log(ba);
-      resolve(ba);
+    try {
+      var query = N1qlQuery.fromString(queryString);
+      bucket.query(query, function(err, res) {
+        if (err) {
+          reject(err);
+        }
+
+        resolve([parseRowQueryResult(res, name)]);
+      });
+    } catch(err) {
+      reject(err);
+    }
+  });
+}
+
+export function listDatabases(manager) {
+  return new Promise((resolve, reject) => {
+    manager.listBuckets(function(err, buckets) {
+      resolve(buckets.map(bucket => bucket.name));
     });
+    //resolve([bucket]);
   });
 }
 
-export function listDatabases(bucket) {
-  return new Promise((resolve, reject) => {
-    resolve([bucket]);
-  });
-}
-
-export function getQuerySelectTop(bucket, limit) {
-  return `SELECT * FROM ${wrapQuery(bucket)} LIMIT ${limit}`;
+export function getQuerySelectTop(bucketName, table, limit) {
+  return `SELECT * FROM ${bucketName} WHERE _type="${table}" LIMIT ${limit}`;
 }
 
 export function getTableCreateScript() {
   return new Promis((resolve, reject) => {
-    resolve("not implemented");
+    resolve("Not implemented!");
   });
 }
 
 export function getViewCreateScript() {
   return new Promise((resolve, reject) => {
-    resolve("not implemented");
+    resolve("Not implemented!");
   });
 }
 
 export function getRoutineCreateScript() {
   return new Promise((resolve, reject) => {
-    resolve("not implemented");
+    resolve("Not implemented!");
   });
 }
 
 export function wrapQuery(item) {
-  return `"${item}"`;
+  return `${item}`;
 }
 
 const getSchema = async (connection) => {
@@ -135,10 +168,8 @@ const getSchema = async (connection) => {
 };
 
 export const truncateAllTables = async (connection) => {
-  resolve("not implemented");
+  resolve("Not implemented!");
 };
-
-
 
 function _configDatabase(server, database) {
   const config = {
@@ -157,19 +188,18 @@ function _configDatabase(server, database) {
   if (server.config.ssl) {
     config.ssl = server.config.ssl;
   }
-
   return config;
 }
 
-
-function parseRowQueryResult(data) {
+function parseRowQueryResult(data, name) {
   const isSelect = Array.isArray(data);
-  let rows = data.map(d => d.default);
-  let fields = Object.keys(data[0].default).map(key => {
-    return { 'name': key}
-  });
-  console.log(rows);
-  console.log(fields);
+  let rows = [];
+  let fields = [];
+  if(data.length > 0) {
+    rows = data.map(d => d[name] || d);
+    fields = Object.keys(data[0][name] || data[0]).map(key => ({ 'name': key}));
+  }
+
   return {
     isSelect,
     rows,
@@ -177,4 +207,15 @@ function parseRowQueryResult(data) {
     rowCount: data.length,
     affectedRows: data.length,
   };
+}
+
+function getFrom(query, bucketName) {
+  const fromStr = query.match(/from [a-z,.]*/gi);
+  if(fromStr.length > 0) {
+    const idx = fromStr[0].lastIndexOf('.');
+    const name = fromStr[0].substring(idx !== -1 ? idx+1 : 5);
+    return name;
+  } else {
+    return bucketName;
+  }
 }
